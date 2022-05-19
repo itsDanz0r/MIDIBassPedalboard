a9cd0108ffe71f6b62f72f39b823e8761d39c851
Danny Kerr <97600638+itsDanz0r@users.noreply.github.com>
2022-05-19 12:04:44 +0930

    Create pedalboard.ino

---
+++ pedalboard.ino
@@ -0,0 +1,79 @@
+const int NOTE_ON = 144;
+const int NOTE_OFF = 128;
+const int PEDAL_COUNT = 2;
+
+int velocity = 100;
+
+
+class Pedal {
+  
+  private:
+    int _pin;
+    bool _state;
+    int _note;
+    
+  public:
+    int pin;
+    bool state;
+    int note;
+    
+  Pedal(int pin = 0, bool _state = 0, int _note = 0) {
+    pin = _pin;
+    state = _state;
+    note = _note;
+  };
+  
+  void changeState() {
+    String print_state;
+    state = !state;
+    
+    if (state == 1) {
+      Serial.write(NOTE_ON);
+      Serial.write(note);
+      Serial.write(velocity);
+    }
+    else if (state == 0) {
+      Serial.write(NOTE_OFF);
+      Serial.write(note);
+      Serial.write(velocity);
+    }
+
+  };
+  
+};
+
+Pedal pedals[PEDAL_COUNT];
+
+void setup() {
+  // Start serial connection at std MIDI baud rate
+  Serial.begin(31250);
+
+  // Loop through each Pedal object in the array and assign sequential pins
+  for (int i = 0; i < PEDAL_COUNT; i++){
+     pedals[i].pin = i+2;
+     pedals[i].note = i+24;
+     pedals[i].state = 0;
+  }  
+}
+
+void check_velocity(){
+  int pot_value = map(analogRead(A0), 0, 1023, 0, 127);
+  if (pot_value != velocity) {
+    velocity = pot_value;
+  }
+}
+
+void read_pedals(){
+
+  for (int i = 0; i < PEDAL_COUNT; i++){
+    if (((digitalRead(pedals[i].pin) == LOW) && (pedals[i].state == 1)) || ((digitalRead(pedals[i].pin) == HIGH) && (pedals[i].state == 0))) {
+      pedals[i].changeState();
+      delay(3);
+    }
+  }
+}
+
+void loop() {
+  check_velocity();
+  read_pedals();
+}
